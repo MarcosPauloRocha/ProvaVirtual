300116bc341dd03885562c1f6a46c2a61b7b1717
MarcosPauloRocha <mpmarcosrocha@gmail.com>
2022-04-27 23:24:14 -0300

    Atualização Projeto.

--- src/main/java/br/com/polidev/provavirtual/controller/generic/GenericController.java
+++
@@ -1,8 +0,0 @@
-package br.com.polidev.provavirtual.controller.generic;
-
-import br.com.polidev.provavirtual.model.generic.GenericBase;
-
-
-public interface GenericController<T extends GenericBase> {
-    
-}
--- src/main/java/br/com/polidev/provavirtual/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.polidev.provavirtual.repository;
-
-import br.com.polidev.provavirtual.model.Usuario;
-import br.com.polidev.provavirtual.repository.generic.GenericRepository;
-
-
-public interface UsuarioRepository extends GenericRepository<Usuario>{
-
-}
